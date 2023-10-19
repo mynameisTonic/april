631620260b9fea0a40d01ec3a410fa0613835f37
MynameisTonic <ginathetran@outlook.com>
2023-10-19 16:25:36 -0600

    Added some temperature things for step 2 and 3

--- temp.py
+++ temp.py
@@ -1,5 +1,17 @@
 #Step 1 - Convert Temperature from Celcius to Fahrenheit
 #Temperature in degrees Fahrenheit (°F) = (Temperature in degrees Celsius (°C) * 9/5) + 32.
-celcius_val = 25
-fahrenheit = celcius_val * 9 / 5 + 32
-print (f"The temeperature is {celcius_val} degrees celcius. That's {fahrenheit} degrees in Fahrenheit!")
+celcius = input("Enter the temperature in celcius>>>") 
+fahrenheit_calc = celcius * 9 / 5 + 32
+print (f"The temeperature is {celcius} degrees celcius. That's {fahrenheit_calc} degrees in Fahrenheit!")
+
+#Step 2 - Function to Convert Temperature from Celcius to Fahrenheit
+def celcius_to_fahrenheit(celcius):
+   print(fahrenheit_calc) 
+
+#Step 3 - Do the same but in reverse
+
+farenheit = input("Enter the temperature in fahrenheit>>>")
+celcius_calc = farenheit - 32 * 5 / 9
+
+def fahrenheit_to_celcius():
+   print(celcius)
