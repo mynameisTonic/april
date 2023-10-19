b971964ed58b83196b6b2f0edd9fa2d4935de456
MynameisTonic <ginathetran@outlook.com>
2023-10-19 16:52:57 -0600

    I made sure my code actually worked

--- temp.py
+++ temp.py
@@ -1,18 +1,23 @@
 #Step 1 - Convert Temperature from Celcius to Fahrenheit
 #Temperature in degrees Fahrenheit (°F) = (Temperature in degrees Celsius (°C) * 9/5) + 32.
-celcius = input("Enter the temperature in celcius>>>") 
-fahrenheit_calc = celcius * 9 / 5 + 32
-print (f"The temeperature is {celcius} degrees celcius. That's {fahrenheit_calc} degrees in Fahrenheit!")
+#celcius = input ("Enter the temperature in celcius>>>") 
+#celcius = int()
+#print (f"The temeperature is {celcius} degrees celcius. That's {fahrenheit_calc} degrees in Fahrenheit!")
 
 #Step 2 - Function to Convert Temperature from Celcius to Fahrenheit
-def celcius_to_fahrenheit(celcius):
-   print(fahrenheit_calc) 
-
-#Step 3 - Do the same but in reverse
-
-farenheit = input("Enter the temperature in fahrenheit>>>")
-celcius_calc = farenheit - 32 * 5 / 9
-
-def fahrenheit_to_celcius():
-   print(celcius)
-   
+tempselect = input("what temperature do you have right now? (F/C)>>>").lower()
+if tempselect == "c":
+    def celcius_to_fahrenheit():
+        celcius = int(input ("Enter the temperature in celcius>>>"))
+        fahrenheit_calc = celcius * 9 / 5 + 32
+        print (f"The temeperature is {celcius} degrees celcius. That's {fahrenheit_calc} degrees in fahrenheit!")
+    celcius_to_fahrenheit()
+    #Step 3 - Do the same but in reverse
+elif tempselect == "f":
+    def fahrenheit_to_celcius():
+        farenheit = int(input("Enter the temperature in fahrenheit>>>"))
+        celcius_calc = farenheit - 32 * 5 / 9
+        print (f"The temeperature is {farenheit} degrees fahrenheit. That's {celcius_calc} degrees in celcius!")
+    fahrenheit_to_celcius()
+else:
+    print("Not a valid response. Please try again later")
