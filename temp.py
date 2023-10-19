5d998caf3c4302fad315a61ebaf944894bc89a6b
MynameisTonic <ginathetran@outlook.com>
2023-10-19 16:03:58 -0600

    Added a new file, and did some coding.

---
+++ temp.py
@@ -0,0 +1,5 @@
+#Step 1 - Convert Temperature from Celcius to Fahrenheit
+#Temperature in degrees Fahrenheit (°F) = (Temperature in degrees Celsius (°C) * 9/5) + 32.
+celcius_val = 25
+fahrenheit = celcius_val * 9 / 5 + 32
+print (f"The temeperature is {celcius_val} degrees celcius. That's {fahrenheit} degrees in Fahrenheit!")
